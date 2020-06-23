e4e119e668f8cd87a4cde56a18f23d2b980b2218
baifcqqq <2834211870@qq.com>
2020-06-23 20:20:02 +0800

    首页布局

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,14 +1,25 @@
 <!-- 首页 -->
 <template>
   <div class="container">
-	  <div class="header">
-      <div class="left">
-        <h1></h1>
-        <div class="position"></div>
+	  <div id="head">
+      <div class="left_dom">
+        <h1 class="fl">
+          <img src="http://statics.zhaogongdi.com/common/logo_m.png" alt="">
+        </h1>
+        <div class="position fl">
+          <i class="icon-dingwei iconfont" /><b>成都</b>
+          <strong class="iconfont icon-youjiantou"></strong>
+        </div>
       </div>
-      <div class="right">
+      <div class="right_dom fr">
+        <a href="https://a.app.qq.com/o/simple.jsp?pkgname=com.yupao.machine"></a>
+      </div>
+    </div>
+    <div class="banner">
 
-      </div>
+    </div>
+    <div class="menus">
+      
     </div>
     <Tarbar />
   </div>
@@ -16,6 +27,7 @@
 
 <script>
 import Tarbar from '../../components/tarbar'
+
 
 export default {
   data(){
@@ -28,12 +40,11 @@ export default {
   },
   methods:{
 
-  },
+  }
 }
 </script>
 
-<style>
-.container{
-  
-}
+<style lang='scss'>
+@import "./style.scss"
+
 </style>
