a7973a6fdf700b6a7362ef41a7e0ecf8be3324f2
ye <841345526@qq.com>
2020-06-25 10:56:29 +0800

    x

---
+++ pages/user/personalSettings.vue
@@ -0,0 +1,20 @@
+<!--个人设置-->
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style lang='scss'>
+@import './personalSettings.scss';
+</style>
