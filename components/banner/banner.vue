7c1c797e148afaab36d78b405d7fc8562129ad76
baifcqqq <2834211870@qq.com>
2020-06-26 14:16:15 +0800

    轮播图封装

---
+++ components/banner/banner.vue
@@ -0,0 +1,34 @@
+<template>
+    <van-swipe class="my-swipe" indicator-color="white" width="7.5rem" :autoplay="3000" ref="resize">
+      <van-swipe-item>1</van-swipe-item>
+      <van-swipe-item>2</van-swipe-item>
+      <van-swipe-item>3</van-swipe-item>
+      <van-swipe-item>4</van-swipe-item>
+    </van-swipe>
+</template>
+
+<script>
+  import { Swipe , SwipeItem } from 'vant';
+  export default{
+    data(){
+      return{
+
+      }
+    },
+    components:{
+      "van-swipe": Swipe,
+      "van-swipe-item" : SwipeItem,
+    },
+    mounted() {
+      // 轮播图重新获取上一级的尺寸
+      setTimeout(()=>{
+        this.$refs.resize.resize()
+      },0)
+      console.log(this)
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
--- components/firstListItem/index.vue
+++ components/firstListItem/index.vue
@@ -2,36 +2,77 @@
   <div class="list-item">
     <div class="title">123456</div>
     <div class="left-img">
-      <img src="../../assets/img/home-images/Lark20200608-172704.png" alt="">
+      <img :src="default_img" alt="">
     </div>
     <div class="right-inner">
       <div class="basic-info">
-         <p class="userName">张大炮</p>
+         <p class="userName">张大dfadfd发到哪里看的积分卡建安费地方撒大家快来房间大炮</p>
          <p class="userTel">18384374***</p>
-         <p class="tel-icon"></p>
+         <p class="tel-icon"  @click="callPhone" v-show="is_mine"></p>
       </div>
-      <div class="type-class overflow_hiding">
-        <a href="javascript:;">123456</a>
-        <a href="javascript:;">123456</a>
-        <a href="javascript:;">123456</a>
+      <div class="type-class">
+        <a href="javascript:;">123fda大发大打发打发456</a>
+        <a href="javascript:;">12发放大3456</a>
+        <a href="javascript:;">1234发的发大发打发打发大是打发打发的56</a>
       </div>
       <div class="position-time">
-
+        <div class="position fl">
+          <i class="icon-dingwei iconfont"></i>
+           西藏 布达拉宫
+        </div>
+        <div class="time fr">
+          2020-05-26 10:00:00
+        </div>
       </div>
     </div>
+    <!-- 弹框 -->
+    <van-dialog class="phone_alert" v-model="show" title="温馨提示" @confirm="go_call" show-cancel-button confirmButtonText="去拨打">
+      <p class="ask">你是否要拨打电话?</p>
+      <div class="tips">
+        <input type="checkbox" v-model="whether">七天内不再提示
+      </div>
+    </van-dialog>
   </div>
 </template>
 
 <script>
+  import { Dialog } from 'vant';
   export default{
+   components:{
+      [Dialog.Component.name]: Dialog.Component,
+   },
     data(){
       return{
-
+        default_img:'http://statics.zhaogongdi.com/common/default_header.png',
+        is_mine:true, //是否是自己发布的消息
+        show:false,  //是否显示弹框
+        whether:false //是否勾选七天内不打电话
       }
     },
     methods:{
+      //打电话弹框显示
+        callPhone(){
+          // 获取本地存储
+          let is_seven = localStorage.getItem('is_seven');
 
-    }
+          if(is_seven == "false" || is_seven == null){
+            this.show = true
+          }else{
+            console.log('dadianh')
+          }
+
+
+        },
+        //点击去拨打按钮
+        go_call(){
+          //勾选了选择框
+          if(this.whether){
+             localStorage.setItem('is_seven',"true")
+          }
+        }
+
+      }
+
   }
 </script>
 
