30b9290428c62c99ebfbd26701fc59e8d105f9c6
ye <841345526@qq.com>
2020-07-11 14:58:07 +0800

    x

--- pages/user/get.vue
+++ pages/user/get.vue
@@ -49,7 +49,7 @@ export default {
     data(){
         return{
             list:[
-                {title:'每日签到 + 1 鱼泡币',detail:'每日一次',btn_text:'',src:''},
+                {title:'每日签到 + 1 鱼泡币',detail:'每日一次',btn_text:'已签到',src:''},
                 {title:'邀请好友成功 + 5 鱼泡币',detail:'好友注册送鱼泡币,  不限次数',btn_text:'去邀请',src:'/user/invitation'},
                  {title:'充值',detail:'充值付钱购买鱼泡币',btn_text:'去充值',src:'/coin/recharge'},
                 {title:'发布求租 + 1鱼泡币',detail:'通过审核后赠送, 5次/月',btn_text:'去发布'},
