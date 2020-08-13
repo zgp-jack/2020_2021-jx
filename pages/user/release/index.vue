7b8903dda383a3a8967c12ab65623870e23888e8
ye <841345526@qq.com>
2020-08-13 14:45:07 +0800

    x

--- pages/user/release/index.vue
+++ pages/user/release/index.vue
@@ -42,7 +42,7 @@
             </van-list>
             <EmptyMsg :empty1="iscomplete && !isempty" v-if="!firstEmpty" />
           </van-pull-refresh>
-           <EmptyMsg :empty2="!isempty" v-if="firstEmpty"/>
+           <EmptyMsg :empty2="isempty" v-if="firstEmpty"/>
            <div class="go-release" v-if="firstEmpty">
               <button @click="goReleasePage">去发布</button>
               <p>你还没有发布消息，点击这里发布</p>
