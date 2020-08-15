a2be5d4c16ef81c07b2caf0e88663aefc1656c06
ye <841345526@qq.com>
2020-08-15 10:05:11 +0800

    x

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,33 +3,33 @@
     params,data(参数)
 */
 
-import {Toast} from 'vant';
+import { Toast } from 'vant';
 import qs from 'qs';
-import {setCookie,isWeixin} from '../static/utils/utils';
+import { setCookie, isWeixin } from '../static/utils/utils';
 
-export default function ({$axios,redirect,app}) {
+export default function({ $axios, redirect, app }) {
     // request拦截器
     let globalLoading;
     $axios.onRequest(config => {
         globalLoading = true;
-        const {params,data} = config;
+        const { params, data } = config;
         let source;
         if (process.browser) {
-            source = isWeixin()?'wx':'M';
+            source = isWeixin() ? 'wx' : 'M';
         }
-        if(!config.url.includes('source')){
-            config.url.includes('?')?config.url += `&source=${source}` : config.url += `?source=${source}`;
+        if (!config.url.includes('source')) {
+            config.url.includes('?') ? config.url += `&source=${source}` : config.url += `?source=${source}`;
         }
 
-        if(params && params.globalLoading === false){
+        if (params && params.globalLoading === false) {
             globalLoading = false
             delete params.globalLoading
         }
-        if(data && data.globalLoading === false){
+        if (data && data.globalLoading === false) {
             globalLoading = false
             delete data.globalLoading
         }
-        if(data && config.url!=='/upload?source=M'){
+        if (data && config.url !== '/upload?source=M') {
             config.data = qs.stringify(config.data)
             config.headers.common['content-type'] = 'application/x-www-form-urlencoded';
         }
@@ -39,23 +39,23 @@ export default function ({$axios,redirect,app}) {
             loadingType: 'spinner',
         });
         let ssoToken = app.$cookies.get('ssoToken');
-        if(ssoToken){
+        if (ssoToken) {
             config.headers.common['x-token'] = ssoToken;
         }
     })
     $axios.onError(error => {
-        Toast.clear()
-        console.log(error)
-    })
-    // response拦截器
+            Toast.clear()
+            console.log(error)
+        })
+        // response拦截器
     $axios.interceptors.response.use(response => {
         Toast.clear()
         if (response.status == 200) {
-            if (response.data.code==300){
+            if (response.data.code == 300) {
                 Toast('账号异常')
             } else if (response.data.code == 0) {
                 if (process.browser) {
-                    setCookie('ssoToken','',-1)
+                    setCookie('ssoToken', '', -1)
                 }
                 redirect({
                     path: '/login',
